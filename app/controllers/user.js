2ca2adecba949bce73edc839d3cab232e4d90af7
SunsetFrost <1262457596@qq.com>
2019-09-03 14:46:54 +0800

    feat: 更新user路由

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -22,7 +22,7 @@ class User {
 
     async signIn(ctx) {
         let data = ctx.request.body;
-        console.log(data);
+
         let result = {
             status: 0,
             msg: '',
@@ -30,7 +30,6 @@ class User {
         }
 
         let userResult = await userService.signIn(data);
-        console.log(userResult);
         if(userResult) {
             if(userResult.name === data.name) {
                 result.status = 1;
@@ -39,8 +38,27 @@ class User {
         } else {
             result.msg = '用户名或密码错误';
         }
+    }
 
-        ctx.body = result;
+    async signUp(ctx) {
+        let data = ctx.request.body;
+
+        let result = await userService.create({
+            ...data,
+            create_time: new Date().getTime(),
+        })
+
+        if(result && result.insertId * 1 > 0) {
+            ctx.body = {
+                status: 200,
+                msg: '注册成功',
+            }
+        } else {
+            ctx.body = {
+                status: 0,
+                msg: '注册失败',
+            }
+        }
     }
 }
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,5 +3,6 @@ const User = require('../controllers/user');
 
 router.get('/', User.getUsers);
 router.post('/login', User.signIn);
+router.post('/register', User.signUp);
 
 module.exports = router;
